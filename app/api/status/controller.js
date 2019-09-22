e6479ac8986769f2c5f99a2897338d375d0707a2
Matt Kapinos <matt@Matts-MBP.fios-router.home>
2019-09-22 19:56:01 -0400

    Finished requirement 1

--- app/api/status/controller.js
+++ app/api/status/controller.js
@@ -1,5 +1,35 @@
+const mongoDB = require('./../../lib/mongodb')
+
 exports.currentStatus = function (req, res) {
-  res.status(200).send({
-    status: 'OK'
-  })
+  switch(mongoDB.readyState) {
+    case 0:
+      DB_status = 'disconnected'
+      res.status(503).send({
+        status: DB_status
+      })
+    break;
+    case 1:
+      DB_status = 'OK'
+      res.status(200).send({
+        status: DB_status
+      })
+    break;
+    case 2:
+      DB_status = 'connecting'
+      res.status(503).send({
+        status: DB_status
+      })
+    break;
+    case 3:
+      DB_status = 'disconnecting'
+      res.status(503).send({
+        status: DB_status
+      })
+    break;
+    default:
+      DB_status = 'unknown connection issues' 
+      res.status(503).send({
+        status: DB_status
+      }) 
+}
 }
